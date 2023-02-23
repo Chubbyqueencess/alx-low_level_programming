99081f8585a2af912fad87b54f002e1a9dc4ba0d
Abigail Ejeh <ejehrichard9@gmail.com>
2023-02-23 13:12:57 -0800

    Task 0

---
+++ 0-isupper.c
@@ -0,0 +1,21 @@
+/*
+ * file: 0-isupper.c
+ * Auth: Abigail Ejeh
+ */
+
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters.
+ * @c: The charcter to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
+}
